535608132ef18dbecc1b9093d69f373afe050334
suvakovan <suvasuvakovan@gmail.com>
2026-02-06 21:53:37 +0530

    remove duplicate

---
+++ remove duplicate.py
@@ -0,0 +1,16 @@
+arr =[1,2,3,4,4,5,6,7,8,9,9]
+unique =[]
+for num in arr:
+    if num not in unique:
+        unique.append(num)
+print(unique)
+
+
+
+arr1 = ['a','b','c','d','d','e','f','g','h','i','i']
+unique1 =[]
+for char in arr1:
+    if char not in unique1:
+        unique1.append(char)
+    print (unique1)
+print(unique1)    
