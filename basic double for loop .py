1585a5f2d24e4bee3044bd31d42a64fbbda60759
suvakovan <suvasuvakovan@gmail.com>
2026-02-09 01:34:48 +0530

    basic double loop

---
+++ basic double for loop .py
@@ -0,0 +1,11 @@
+for i in range(3):
+    for j in range (3):
+        print(i)
+        print(j)
+        print(i,j)
+
+
+
+        #  i for the outer loop    example output as  0,0,0,1,1,1,2,2,2
+        #  j for the inner loop    example output as  1,2,3,1,2,3,1,2,3
+        # i,j both in one loop     if both means its all come in one
